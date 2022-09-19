11247c63b7b5e553c2554d1b09661d2d0bb03dfc
markoclimako <mkclimako@gmail.com>
2022-09-19 12:50:07 -0300

    feat(component): create GameBanner component

--- src/components/GameBanner/index.tsx
+++ src/components/GameBanner/index.tsx
@@ -1,3 +1,5 @@
+
+
 interface GameBannerProps {
 bannerUrl: string;
 title: string;
@@ -8,7 +10,7 @@ export function GameBanner(props: GameBannerProps) {
   return (
     <a href="#" className="relative rounded-lg overflow-hidden">
       <img src={props.bannerUrl} alt="" />
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+      <div className="w-full pt-16 pb-4 px- bg-game-gradient absolute bottom-0 left-0 right-0">
         <h3 className="font-semibold text-white block"> {props.title}</h3>
         <span className="text-zinc-300 text- block">{props.adsCount} anúncio(s)</span>
       </div>
